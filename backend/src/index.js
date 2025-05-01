a18a6baa9baafe7ac57755a729bb3799d3dfa33a
Rea <ballkocirea@gmail.com>
2025-05-01 11:34:37 +0200

    feat: install dotenv to use .env file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 const app = express()
+
+dotenv.config()
 
 app.use(cors())
 app.use(express.json())
 
-const PORT = 3001
-
 app.get('/', (req, res) => res.send('Hello World!'))
 
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
